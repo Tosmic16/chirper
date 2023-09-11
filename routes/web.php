5e0aa2ea3156deefbe25f578e9baf7070d7ae195
Tosin <tosin.vincent@trustvas.com>
2023-09-11 09:16:16 +0100

    configured route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/chirps', [ChirpController::class, 'index'])
+->middleware(['auth', 'verified'])
+->name('chirps');
+
 
 require __DIR__.'/auth.php';
