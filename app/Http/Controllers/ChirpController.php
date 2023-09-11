328560d512056655936184a8fb328a9940b89a90
Tosin <tosin.vincent@trustvas.com>
2023-09-11 10:47:59 +0100

    reeady to chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
-    //
+    public function index () : View
+    {
+        return view('chirps',[
+            
+        ]);
+    }
 }
